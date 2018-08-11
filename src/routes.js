6ab416d6c9c76985e7c996a32b3a0ffb88585c04
TheInfRiNgeROfCalmNeSs <theinfringerofcalmness@gmail.com>
2018-08-12 00:13:20 +0300

    comment to routes.js has been added

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@
 
 let moduleName = location.pathname.slice(1).split(".")[0]
 
+// the next step to rewrite requiring modules with require.ensure()
 let route = moduleName!==""?require.context('./routes', false)(`./${moduleName}`):(()=>console.log('route not provided'))
 
 //console.log('moduleName', moduleName, 'location', location, 'route', route, 'typeof route', typeof route)
